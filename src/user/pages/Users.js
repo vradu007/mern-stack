55a51a7ac3cd12520f1bb5f3b32bb3c15794599f
vradu007 <radu.voin@gmail.com>
2021-02-07 16:32:01 +0200

    added UserItem component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import UsersList from '../components/UsersList';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    const USERS = [{id: 'u1', name:'name1', image="https://content.fortune.com/wp-content/uploads/2017/01/google.jpeg", places: 3}];
-	return <UsersList items={USERS}/>
+	const USERS = [
+		{
+			id: "u1",
+			name: "name1",
+			image:
+				"https://content.fortune.com/wp-content/uploads/2017/01/google.jpeg",
+			places: 3,
+		},
+	];
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
