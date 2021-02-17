26322cc48eb13545929918c1878be02ebb42fa03
vradu007 <radu.voin@gmail.com>
2021-02-18 01:06:01 +0200

    created the user places page and added dynamic routing for it

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
+import UserPlaces from './places/pages/UserPlaces'
 
 const App = () => {
 	return (
@@ -18,6 +19,9 @@ const App = () => {
 					<Route path="/" exact>
 						<Users />
 					</Route>
+                    <Route path="/:userId/places" exact>
+                        <UserPlaces/>
+                    </Route>
 					<Route path="/places/new">
 						<NewPlace />
 					</Route>
